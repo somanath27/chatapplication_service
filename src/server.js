06a39697843b672943448d9227c496fcc88bff9b
Somanath Barik <somanathbarik143@gmail.com>
2024-03-29 20:18:35 +0530

    some changes done

--- src/server.js
+++ src/server.js
@@ -57,7 +57,7 @@ const server = app.listen(PORT, () => {
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.TEST_DB_PATH)
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (err) {
         console.log(`Error: ${err.message}`)
